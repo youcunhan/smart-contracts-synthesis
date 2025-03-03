b78a36d49054f4288fc5a374623160e3493a2d1a
youcunhan <youch@shanghaitech.edu.cn>
2025-03-04 03:05:04 +0800

    bmc debug

--- src/main/scala/synthesis/BoundedModelChecking.scala
+++ src/main/scala/synthesis/BoundedModelChecking.scala
@@ -1,65 +1,87 @@
 package synthesis
 
-import com.microsoft.z3._
+import com.microsoft.z3.{BoolExpr, BoolSort, Context, Expr, Solver, Sort, Status}
 
 object BoundedModelChecking {
+  private var index: Int = 0
 
-  var index = 0
-  def fresh(s: Sort, ctx: Context): Expr = {
-    index += 1
-    ctx.mkConst(s"!f$index", s)
+  def pureName(name: String): String = {
+    if (name.contains("|")) name.substring(0, name.indexOf('|')) else name
   }
 
-  def zipp[T, U](xs: List[T], ys: List[U]): List[(T, U)] = {
+  def fresh(round: Int, ctx: Context, name: String, sort: Sort): Expr[_] = {
+    index += 1
+    ctx.mkConst(s"${name}|r${round}|i${index}", sort)
+  }
+
+  def zipp(xs: Array[Expr[_]], ys: Array[Expr[_]]): Array[(Expr[_], Expr[_])] = {
     xs.zip(ys)
   }
 
-  def bmc(init: BoolExpr, trans: BoolExpr, goal: BoolExpr, fvs: List[Expr], xs: List[Expr], xns: List[Expr], ctx: Context): Unit = {
+  def bmc(ctx: Context, init: BoolExpr, trans: BoolExpr, goal: BoolExpr,
+          fvs: Array[Expr[_]], xs: Array[Expr[_]], xns: Array[Expr[_]]): Option[Map[String, Expr[_]]] = {
     val solver = ctx.mkSolver()
+    // solver.reset("timeout", 2000)
     solver.add(init)
+    
     var count = 0
+    var currentTrans = trans
+    var currentGoal = goal
+    var currentXs = xs
+    var currentXns = xns
+    var currentFvs = fvs
 
-    while (true) {
-      println(s"iteration $count")
+    while (count <= 7) {
       count += 1
+      val p = fresh(count, ctx, "P", ctx.getBoolSort).asInstanceOf[BoolExpr]
+      solver.add(ctx.mkImplies(p, currentGoal))
 
-      val p = fresh(ctx.mkBoolSort(), ctx)
-      solver.add(ctx.mkImplies(p.asInstanceOf[BoolExpr], goal))
-
-      if (solver.check() == Status.SATISFIABLE) {
-        println(s"Model: ${solver.getModel}")
-        return
+      val res = solver.check(p)
+      if (res == Status.SATISFIABLE) {
+        return Some(extractModel(solver.getModel))
       }
+      solver.add(currentTrans)
 
-      solver.add(trans)
-      val ys = xs.map(x => fresh(x.getSort, ctx))
-      val nfvs = fvs.map(x => fresh(x.getSort, ctx))
+      val ys = currentXs.map(x => fresh(count, ctx, pureName(x.toString), x.getSort))
+      val nfvs = currentFvs.map(f => fresh(count, ctx, pureName(f.toString), f.getSort))
 
-      val newTrans = substitute(trans, zipp(xns ++ xs ++ fvs, ys ++ xns ++ nfvs))
-      val newGoal = substitute(goal, zipp(xs, xns))
-
-      bmc(init, newTrans, newGoal, nfvs, ys, xns, ctx)
+      currentTrans = currentTrans.substitute(
+        zipp(currentXns ++ currentXs ++ currentFvs, ys ++ currentXns ++ nfvs): _*
+      ).asInstanceOf[BoolExpr]
+      
+      currentGoal = currentGoal.substitute(zipp(currentXs, currentXns): _*).asInstanceOf[BoolExpr]
+      
+      currentXs = currentXns
+      currentXns = ys
+      currentFvs = nfvs
     }
+    None
   }
 
-  def substitute(expr: Expr, bindings: List[(Expr, Expr)]): Expr = {
-    var result = expr
-    for ((oldExpr, newExpr) <- bindings) {
-      result = result.substitute(oldExpr, newExpr)
+  def extractModel(model: com.microsoft.z3.Model): Map[String, Expr[_]] = {
+    var extractedModel = Map[String, Expr[_]]()
+    val iter = model.getConstDecls.iterator
+    
+    while (iter.hasNext) {
+      val decl = iter.next()
+      val name = decl.getName.toString
+      val value = model.getConstInterp(decl)
+      extractedModel += (pureName(name) -> value)
     }
-    result
+    extractedModel
   }
 
   def testbmc(): Unit = {
     val ctx = new Context()
-
-    val x0 = ctx.mkConst("x0", ctx.mkBitVecSort(4))
-    val x1 = ctx.mkConst("x1", ctx.mkBitVecSort(4))
-
-    val init = ctx.mkEq(x0, ctx.mkInt(0))        // x0 == 0
-    val trans = ctx.mkEq(x1, ctx.mkAdd(x0, ctx.mkInt(3)))  // x1 == x0 + 3
-    val goal = ctx.mkEq(x0, ctx.mkInt(10))        // x0 == 10
-
-    bmc(init.asInstanceOf[BoolExpr], trans.asInstanceOf[BoolExpr], goal.asInstanceOf[BoolExpr], List(), List(x0), List(x1), ctx)
+    val bvSize = 8
+    val bvSort = ctx.mkBitVecSort(bvSize)
+    val x = ctx.mkBVConst("x", bvSize)
+    val xn = ctx.mkBVConst("x'", bvSize)
+    val init = ctx.mkEq(x, ctx.mkBV(0, bvSize))
+    val trans = ctx.mkEq(xn, ctx.mkBVAdd(x, ctx.mkBV(1, bvSize)))
+    val goal = ctx.mkEq(x, ctx.mkBV(5, bvSize))
+    
+    val result = bmc(ctx, init, trans, goal, Array(), Array(x), Array(xn))
+    println(result.getOrElse("No model found"))
   }
 }
